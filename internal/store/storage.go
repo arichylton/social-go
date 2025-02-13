63df538a34cdbcf5b17e14ddf705218197423d8d
Aric Hylton <Arich212@gmail.com>
2025-02-12 19:08:46 -0600

    added get posts and add posts

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,11 +3,15 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 )
+
+var ErrorNotFound = errors.New("resouce not found")
 
 type Storage struct {
 	Posts interface {
 		Create(context.Context, *Post) error
+		GetByID(context.Context, int64) (*Post, error)
 	}
 	Users interface {
 		Create(context.Context, *User) error
